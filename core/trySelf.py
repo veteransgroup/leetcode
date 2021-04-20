f4368d391f92745bc807145a4f385f8f7269c22a
Jeff Liu <Jeff.Liu@outlook.co.nz>
2021-04-20 16:22:23 +1200

    adjust code

--- core/trySelf.py
+++ core/trySelf.py
@@ -13,19 +13,17 @@ def fibonacci(num: int):
     for i in range(num-2):
         back1, back2 = back2, back2+back1
         fibo.append(back2)
-    if num < 2:
-        fibo = fibo[:1]
-    return fibo
+    return fibo[:num]
 
 
 # 闭包函数 求 斐波那契数列
-def closure_fibo():
+def closure_fibo(b1, b2):
     """求斐波那契数列
 
     Returns:
         int: 斐波那契数列中的某一位的值
     """
-    b1, b2 = 0, 1
+    # b1, b2 = 0, 1
 
     def next():
         nonlocal b1, b2
@@ -38,8 +36,8 @@ def closure_fibo():
 # print(fibonacci(int(input())))
 
 f1 = fibonacci
-print(f1(10))
+print(f1(1))
 
-f2 = closure_fibo()
+f2 = closure_fibo(3, 7)
 for i in range(10):
     print(f2())
--- leetcode/3Sum.py
+++ leetcode/3Sum.py
@@ -1,6 +1,16 @@
 from collections import Counter
 
+
 def speedup(numbers):
+    """ list 里任意三个数相加为0
+    Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+
+    Args:
+        numbers (list): 一个全部元素为整数的list
+
+    Returns:
+        list: 返回list的里面包含所有情况：任意三个元素相加等于0的子list
+    """
     result = [[0, 0, 0]] if numbers.count(0) > 2 else []
     ocur_numbers = Counter(numbers)
     twice_numbers = [x for x, y in ocur_numbers.items() if y > 1]
@@ -23,7 +33,8 @@ def speedup(numbers):
                     else:
                         used_number.add(inter)
                         used_number.add(index)
-                        result.append([ordered_numbers[outer], ordered_numbers[inter], need])
+                        result.append(
+                            [ordered_numbers[outer], ordered_numbers[inter], need])
 
     for match_twice in twice_numbers:
         need = -2 * match_twice
